7b8114d83c88116b5ffced6bcdfc28406c147af9
nikolakis1919 <96141261+nikolakis1919@users.noreply.github.com>
2022-08-19 19:56:22 +0300

    Update roktracker.bat

--- roktracker.bat
+++ roktracker.bat
@@ -1,6 +1,7 @@
 ".\platform-tools\adb.exe" kill-server
 ".\platform-tools\adb.exe" connect localhost:5555
 cd .\
+pip install Pillow==8.4.0
 pip install -r requirements.txt
 python roktracker.py
 PAUSE
