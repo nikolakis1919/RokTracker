13cd4d836052694151b92b18eac0f9f3349ab392
nikolakis1919 <96141261+nikolakis1919@users.noreply.github.com>
2022-09-21 13:03:58 +0300

    Update roktracker.bat

--- roktracker.bat
+++ roktracker.bat
@@ -1,7 +1,9 @@
 ".\platform-tools\adb.exe" kill-server
 ".\platform-tools\adb.exe" connect localhost:5555
 cd .\
+pip install --upgrade pip
 pip install Pillow==8.4.0
+pip install opencv-python
 pip install -r requirements.txt
 python roktracker.py
 PAUSE
