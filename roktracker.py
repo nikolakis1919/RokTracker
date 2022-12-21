04ebd8f141bd95f51f4ff3f17b1a277a0faec001
nikolakis1919 <96141261+nikolakis1919@users.noreply.github.com>
2022-12-22 00:39:27 +0200

    Update roktracker.py

--- roktracker.py
+++ roktracker.py
@@ -17,7 +17,7 @@ from neural_network import read_ocr
 import requests
 import webbrowser
 
-version = "RokTracker-v7.0"
+version = "RokTracker-v7.1"
 def tointcheck(element):
 	try:
 		return int(element)
@@ -255,19 +255,19 @@ try:
 					f.write(image)
 		image2 = cv2.imread('kills_tier.png') 	
 		image2 = cv2.fastNlMeansDenoisingColored(image2,None,20,20,7,21) 
-		roi = (863, 597, 215, 26) #tier 1
+		roi = (863, 466, 215, 26) #tier 1
 		im_kills_tier1 = image2[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-		roi = (863, 642, 215, 26) #tier 2
+		roi = (863, 511, 215, 26) #tier 2
 		im_kills_tier2 = image2[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-		roi = (863, 687, 215, 26) #tier 3
+		roi = (863, 556, 215, 26) #tier 3
 		im_kills_tier3 = image2[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-		roi = (863, 732, 215, 26) #tier 4
+		roi = (863, 601, 215, 26) #tier 4
 		im_kills_tier4 = image2[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-		roi = (863, 777, 215, 26) #tier 5
+		roi = (863, 646, 215, 26) #tier 5
 		im_kills_tier5 = image2[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
 		#More info tab
