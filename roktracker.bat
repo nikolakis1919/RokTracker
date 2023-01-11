b0205eb25cd403997e8ab540af8c11dfcd0174a6
nikolakis1919 <96141261+nikolakis1919@users.noreply.github.com>
2023-01-11 17:26:04 +0200

    Update roktracker.bat

--- roktracker.bat
+++ roktracker.bat
@@ -3,7 +3,7 @@
 cd .\
 pip install --upgrade pip
 pip install Pillow==8.4.0
-pip install opencv-python
+pip install opencv-python==4.6.0.66
 pip install -r requirements.txt
 python roktracker.py
 PAUSE
