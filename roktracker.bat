1619998e330781b0c0116604e62364c5df65f8a2
nikolakis1919 <96141261+nikolakis1919@users.noreply.github.com>
2023-07-10 12:16:12 +0300

    Update roktracker.bat

--- roktracker.bat
+++ roktracker.bat
@@ -1,16 +1,7 @@
 ".\platform-tools\adb.exe" kill-server
 ".\platform-tools\adb.exe" connect localhost:5555
 cd .\
-REM Check if pip is installed
-pip --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo pip is not installed, installing...
-    python -m ensurepip --default-pip
-    if %errorlevel% neq 0 (
-        echo Failed to install pip, please install manually
-        exit /b
-    )
-)
+pip --version
 pip install --upgrade pip
 pip install Pillow==8.4.0
 pip install opencv-python==4.6.0.66
